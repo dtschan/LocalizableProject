47c711eeb27e823721965da15e2b1a3bffeec8ac
Alessandro Zoffoli <a.zoffoli@apexnet.it>
2014-07-11 10:13:53 +0200

    Initial project.

---
+++ LocalizableProject/LocalizableProject/ALEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ALEAppDelegate.h
+//  LocalizableProject
+//
+//  Created by Alessandro Zoffoli on 11/07/14.
+//  Copyright (c) 2014 Apexnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
